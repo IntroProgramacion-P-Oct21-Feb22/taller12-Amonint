cdd48d53cec6857dbc37b0798919e905c2d3599f
Amon <you@example.com>
2022-01-23 17:59:24 -0500

    .............

--- Ejercicios/src/ejercicios/Problema04.java
+++ Ejercicios/src/ejercicios/Problema04.java
@@ -16,6 +16,59 @@ public class Problema04 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[] bases = {100, 90, 80, 70, 60};
+        double[] potencias = {2, 3, 2, 2, 1};
+        double[] resultado = new double[5];
+        double[] suma = new double[5];
+
+        for (int i = 0; i < bases.length; i++) {
+            double potencia = obtenerPotencia(bases[i], potencias[i]);
+            resultado[i] = potencia;
+
+            double sumaR = obtenerSuma(bases[i], potencias[i]);
+            suma[i] = sumaR;
+
+            System.out.printf("La suma de %.2f + %.2f es: %.2f\tCon potencia "
+                    + "de : %.2f\n",
+                    bases[i], potencias[i], suma[i], resultado[i]);
+        }
+        double sumaArreglo = obtenerSumaArreglo(resultado[0], resultado[1],
+                resultado[2], resultado[3], resultado[4]);
+        System.out.printf("\nLa suma de las posiciones del"
+                + " arreglo resultado es: %.2f", sumaArreglo);
+    }
+
+    public static double obtenerSuma(double a, double b) {
+        if (b == 0) {
+            return a;
+        } else {
+            if (a == 0) {
+                return b;
+            } else {
+                return 1 + obtenerSuma(a, b - 1);
+            }
+        }
+
+    }
+
+    public static double obtenerPotencia(double base, double exponente) {
+
+        if (exponente == 1) {
+            return base;
+        } else {
+
+            return base * obtenerPotencia(base, exponente - 1);
+        }
+    }
+
+    public static double obtenerSumaArreglo(double a, double b, double c,
+            double d, double e) {
+        if (b == 0) {
+            return a;
+
+        } else {
+            return 1 + obtenerSuma(a + b + c+d, e - 1);
+        }
     }
     
 }
